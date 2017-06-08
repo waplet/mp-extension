9b85711ec32aa96857ecec3864f5584442204abe
waplet <waplet@gmail.com>
2017-06-08 15:35:43 +0300

    Now the site will remember for 10 minutes the latest posts

--- content.js
+++ content.js
@@ -1,9 +1,11 @@
 /**
  * Created by waplet on 28/07/16.
  */
-chrome.storage.sync.get("w_last_seen", function(lastSeenObject) {
+chrome.storage.sync.get(["w_last_seen", "w_temp_last_seen"], function(lastSeenObject) {
     // acquired time
     var lastSeenTime = moment(lastSeenObject.w_last_seen || 0, "X");
+    var temporaryLastSeen = lastSeenObject.w_temp_last_seen || null;
+
     // getting all last posts
 
     var lastPosts = document
@@ -12,6 +14,7 @@ chrome.storage.sync.get("w_last_seen", function(lastSeenObject) {
 
     var lastPostsArr = [].slice.call(lastPosts); // hacks to turn HTMLCollection into Array
 
+    var wasNewPost = false;
     lastPostsArr.forEach(function (post, index) {
         // getting post time
         var postTime = post
@@ -26,11 +29,27 @@ chrome.storage.sync.get("w_last_seen", function(lastSeenObject) {
                 .getElementsByTagName('td')[0]
                 .getElementsByClassName('b11')[0];
             postNode.innerHTML = '<span style="color:green;">[NEW]</span> ' + postNode.innerHTML;
+
+            if (!wasNewPost) {
+                wasNewPost = true;
+            }
         }
     })
 
-    // saving visit time
-    chrome.storage.sync.set({w_last_seen: moment().format("X")});
+    // We should set new last seen if if "temprorary" last seen exceeds 10 minutes, or some seconds
+    if (temporaryLastSeen || !wasNewPost) {
+        if (moment().diff(moment(temporaryLastSeen, "X"), 'minutes') > 10 || !wasNewPost) {
+            chrome.storage.sync.set({
+                w_last_seen: moment().format("X"),
+                w_temp_last_seen: null
+            });
+        }
+    } else {
+        chrome.storage.sync.set({
+            w_last_seen: moment().format("X"),
+            w_temp_last_seen: moment().format("X")
+        });
+    }
 });
 
 
